bb14886667ffadd9f6c54338a3e1bdaa359e52b8
TjanMichela <64552249+TjanMichela@users.noreply.github.com>
2023-05-18 16:28:34 -0400

    create card-list.component.jsx

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,18 +1,22 @@
 import { Component } from "react";
 
-class CardList extends Component{
-    render(){
-        console.log(this.props.monsters)
-        console.log('render from CardList')
-        const {monsters} = this.props;
-        return(
-            <div>
-                {monsters.map(monster =>
-                    <h1 key={monster.id}>{monster.name}</h1>    
-                )}
-            </div>
-        )
-    };
+import "./card-list.styles.css";
+import CardContainer from "../card-container/card-container.component";
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return (
+          <CardContainer monster={monster} />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default CardList;
