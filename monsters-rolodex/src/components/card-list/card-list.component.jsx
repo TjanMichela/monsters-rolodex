49768d78826c63f3c16c89639ec50242831a67f7
TjanMichela <64552249+TjanMichela@users.noreply.github.com>
2023-05-23 17:07:33 -0400

    update card-list.component.jsx

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,22 +1,30 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 import "./card-list.styles.css";
 import CardContainer from "../card-container/card-container.component";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <CardContainer monster={monster} key={monster.id}/>
+    })} 
+  </div>
+);
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return (
-          <CardContainer monster={monster} key={monster.id}/>
-          );
-        })}
-      </div>
-    );
-  }
-}
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props;
+
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           return (
+//           <CardContainer monster={monster} key={monster.id}/>
+//           );
+//         })} 
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
