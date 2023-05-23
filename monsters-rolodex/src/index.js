36612e425ec4bae3d0687b74112a3640ba814702
TjanMichela <64552249+TjanMichela@users.noreply.github.com>
2023-05-23 17:07:07 -0400

    update index.js

--- monsters-rolodex/src/index.js
+++ monsters-rolodex/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+// major change Reactv18 use createRoot instead of the above code
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
